9b383771f1ac21fff16a1067c4eb3b40d16b0f18
Alexandre Guerville <alexandre.guerville@techex.co.uk>
2024-10-26 20:13:14 +0100

    feat: Implement paeth predictor function

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 #![allow(dead_code)]
+
 const fn generate_crc_table() -> [u64; 256] {
     let mut res = [0_u64; 256];
     let (mut n, mut k) = (0, 0);
@@ -34,6 +35,19 @@ const ADAM_7_PATTERN: [[u8; 8]; 8] = [
     [5, 6, 5, 6, 5, 6, 5, 6],
     [7; 8],
 ];
+
+fn paeth_predictor(a: u8, b: u8, c: u8) -> u8 {
+    let (a, b, c) = (a as i16, b as i16, c as i16);
+    let p: i16 = a + b - c;
+    let (pa, pb, pc): (i16, i16, i16) = ((p - a).abs(), (p - b).abs(), (p - c).abs());
+    match pa <= pb && pa <= pc {
+        true => pa as u8,
+        false => match pb <= pc {
+            true => pb as u8,
+            false => pc as u8,
+        },
+    }
+}
 
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
