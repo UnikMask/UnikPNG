dfaf886164762f97b445c3feedd7c2afe74ebf8b
UnikMask <alexandre.guerville@hotmail.com>
2024-10-26 11:36:07 +0100

    feat(lib): Build CRC table at compile-time

---
+++ src/lib.rs
@@ -0,0 +1,40 @@
+#![allow(dead_code)]
+const fn generate_crc_table() -> [u64; 256] {
+    let mut res = [0_u64; 256];
+    let (mut n, mut k) = (0, 0);
+    loop {
+        let mut c: u64 = n;
+        loop {
+            c = match c & 1 {
+                1 => 0xedb88320_u64 ^ (c >> 1),
+                _ => c >> 1
+            };
+            k += 1;
+            if k >= 8 {
+                break;
+            }
+        }
+        res[n as usize] = c;
+        n += 1;
+        if n >= 256 {
+            break;
+        }
+    }
+    res
+}
+const CRC_TABLE: [u64; 256] = generate_crc_table();
+
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
